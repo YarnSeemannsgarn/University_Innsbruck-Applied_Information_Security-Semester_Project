99487fb325ca61407cc14e16d0995d67eb38397f
YarnSeemannsgarn <janschlenker1990@gmail.com>
2016-11-16 17:26:45 +0100

    Uses data for products from database

--- vulnerable_web_application/listing.php
+++ vulnerable_web_application/listing.php
@@ -1,75 +1,68 @@
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-	  <!-- Compiled and minified CSS -->
-	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
-	  <link rel="stylesheet" href="assets/css/style.css">
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
 
-     
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+$pdo = new PDO('sqlite:database.sqlite');
+    
+$statement = $pdo->prepare("SELECT * FROM products");
+$result = $statement->execute();
+$products = $statement->fetchAll();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+	    <!-- Compiled and minified CSS -->
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+	    <link rel="stylesheet" href="assets/css/style.css">
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
     <body class="blue-grey lighten-5">
-      
-      
-    <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Vulnerable App!</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-  
-        <li><a href="logout.php">Logout</a></li>
-      </ul>
-    </div>
-  </nav>
-		      
-			
-	<div class="row listing-row">
-		<div class="col s12 m10 offset-m1 ">  
-			<div class="card-panel">     
-        <table class=" highlight stripped responsive-table listing">
-        <thead>
-          <tr>
-              <th data-field="id">Name</th>
-              <th data-field="name">Description</th>
-              <th data-field="price"> Price</th>
-              <th data-field="edit"> Edit</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>Alvin</td>
-            <td>Eclair Lorem ipsum dolor </td>
-            <td>$0.87</td>
-            <td><a class="waves-effect waves-light btn btn-small green darken-2" href="#">Edit</a> <a class="waves-effect waves-light btn btn-small red darken-2" href="#">Delete</a></td>
-          </tr>
-          <tr>
-            <td>Alan</td>
-            <td>Jellybean  Lorem ipsum dolor</td>
-            <td>$3.76</td>
-            <td><a class="waves-effect waves-light btn btn-small green darken-2" href="#">Edit</a> <a class="waves-effect waves-light btn btn-small red darken-2" href="#">Delete</a></td>
-          </tr>
-          <tr>
-            <td>Jonathan</td>
-            <td>Lollipop Lorem ipsum dolor </td>
-            <td>$7.00</td>
-            <td><a class="waves-effect waves-light btn btn-small green darken-2" href="#">Edit</a> <a class="waves-effect waves-light btn btn-small red darken-2" href="#">Delete</a></td>
-          </tr>
-        </tbody>
-      </table>
-        </div>
-         </div>
-    </div>
-   
-      
-      
-      <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <!-- Compiled and minified JavaScript -->
-	  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Vulnerable App!</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+	    <div class="row listing-row">
+		    <div class="col s12 m10 offset-m1 ">  
+			    <div class="card-panel">     
+                    <table class=" highlight stripped responsive-table listing">
+                        <thead>
+                            <tr>
+                                <th data-field="name">Name</th>
+                                <th data-field="description">Description</th>
+                                <th data-field="price"> Price</th>
+                                <th data-field="edit"> Edit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($products as $product): ?>
+                            <tr>
+                                <td><?php echo $product['name']; ?></td>
+                                <td><?php echo $product['description']; ?></td>
+                                <td><?php echo number_format(floatval($product['price']), 2) . "€"; ?></td>
+                                <td><a class="waves-effect waves-light btn btn-small green darken-2" href="#">Edit</a> <a class="waves-effect waves-light btn btn-small red darken-2" href="#">Delete</a></td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>        
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <!-- Compiled and minified JavaScript -->
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
     </body>
-  </html>
+</html>
