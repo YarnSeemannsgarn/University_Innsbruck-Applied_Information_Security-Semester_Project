2dc2a72f5186bdf9e577998f8f76d4b034575d99
Richard Dvorsky <generator@mydevelops.com>
2016-11-20 14:08:57 +0100

    protection against non signed person and split for header and footer

--- vulnerable_web_application/listing.php
+++ vulnerable_web_application/listing.php
@@ -2,38 +2,29 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 session_start();
+
+if(!isset($_SESSION['userid'])) {
+    header("Location: index.php");
+    exit;
+}
 
 $pdo = new PDO('sqlite:database.sqlite');
     
 $statement = $pdo->prepare("SELECT * FROM products");
 $result = $statement->execute();
 $products = $statement->fetchAll();
+
+
+
+
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <!--Import Google Icon Font-->
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--Import materialize.css-->
-	    <!-- Compiled and minified CSS -->
-	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
-	    <link rel="stylesheet" href="assets/css/style.css">
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+ <?
+	 include_once("header.php");
+ ?>
 
-    <body class="blue-grey lighten-5">
-        <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo">Vulnerable App!</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    
-                    <li><a href="logout.php">Logout</a></li>
-                </ul>
-            </div>
-        </nav>
 	    <div class="row listing-row">
 		    <div class="col s12 m10 offset-m1 ">  
 			    <div class="card-panel">     
@@ -57,12 +48,19 @@ $products = $statement->fetchAll();
                             <?php endforeach; ?>
                         </tbody>
                     </table>
+                    </br>
+                    <div class="row">
+	                	<div class="col s12 m10">
+		                	 <a class="waves-effect waves-light btn btn-small blue darken-2" href="#">Add</a>
+		                </div>
+	                </div>
                 </div>
             </div>
-        </div>        
-        <!--Import jQuery before materialize.js-->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <!-- Compiled and minified JavaScript -->
-	    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-    </body>
-</html>
+            
+        </div>   
+        
+ <?
+	 include_once("footer.php");
+ ?>
+             
+ 
