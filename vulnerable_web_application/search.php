204037a56e4c903dbfba05b5af545c2bcf55b95a
Richard Dvorsky <generator@mydevelops.com>
2016-11-20 16:25:52 +0100

    search info

--- vulnerable_web_application/search.php
+++ vulnerable_web_application/search.php
@@ -5,28 +5,51 @@ include_once("init.php");
 $search = $_GET['keyword'];
 
 $pdo = new PDO('sqlite:database.sqlite');
-    
+  
+ 
 $query = "SELECT * FROM products WHERE id LIKE '%".$search."%' 
-												OR  name LIKE '%".$search."%' 
-												OR  description LIKE '%".$search."%' 
-												OR  price LIKE '%".$search."%'";
-$statement = $pdo->prepare($query);
-$result = $statement->execute();
-$products = $statement->fetchAll();
+														OR  name LIKE '%".$search."%' 
+														OR  description LIKE '%".$search."%' 
+														OR  price LIKE '%".$search."%'
+														";
+
+
+$query = "SELECT * FROM products WHERE id LIKE '%".$search."%'";
+
+echo "<pre>".$query."</pre>";
+
+try {	
+		$statement = $pdo->prepare($query); 
+		$result = $statement->execute();
+		$products = $statement->fetchAll();
+    } catch (PDOException $e){
+        echo ('DB Error');
+    }
+
+
+
+
 
 
 
 
 ?>
-
  <?
 	 include_once("header.php");
  ?>
+<pre><?=$query?></pre>
 
 	    <div class="row listing-row">
 		    <div class="col s12 m10 offset-m1 ">  
 			    <div class="card-panel">     
 				    <h4>Search results for: <?=$search?></h4>
+				    <?php 
+					    if(count($products)==0) { 
+						    echo  "No results men!"; 
+						}
+						else{
+						
+				    ?>
                     <table class=" highlight stripped responsive-table listing">
                         <thead>
                             <tr>
@@ -37,23 +60,20 @@ $products = $statement->fetchAll();
                             </tr>
                         </thead>
                         <tbody>
-	                        <?= (count($products)==0 ? "No results!" : "")?>
+	                        
                             <?php foreach($products as $product): ?>
                             <tr>
                                 <td><?php echo $product['name']; ?></td>
                                 <td><?php echo $product['description']; ?></td>
                                 <td><?php echo number_format(floatval($product['price']), 2) . "€"; ?></td>
-                                <td><a class="waves-effect waves-light btn btn-small green darken-2" href="#">Edit</a> <a class="waves-effect waves-light btn btn-small red darken-2" href="#">Delete</a></td>
+                                <td><a class="waves-effect waves-light btn btn-small grey darken-2"  href="javascript:document.location = 'product.php?id=<?=$product["id"]?>';" style="cursor: pointer;">Info</a> </td>
                             </tr>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
-                    </br>
-                    <div class="row">
-	                	<div class="col s12 m10">
-		                	 <a class="waves-effect waves-light btn btn-small blue darken-2" href="#">Add</a>
-		                </div>
-	                </div>
+                    <?php
+	                    }
+                    ?>
                 </div>
             </div>
             
